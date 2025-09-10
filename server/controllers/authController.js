4ec585110db3716719e3d88dd4c7f5ff23fe5211
SaiSree <saisreelikhitha9@gmail.com>
2025-09-10 14:38:37 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,7 @@
 const { Users } = require('../models');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 
 
 exports.signup = async (req, res) => {
