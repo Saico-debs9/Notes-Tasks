585cb71d052e0a4d8b41793e568f85fc8f473b1a
SaiSree <saisreelikhitha9@gmail.com>
2025-09-11 19:13:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,22 +7,22 @@ const { sequelize } = require('./models');
 app.use(cors());
 app.use(express.json());
 
-// const authRoutes = require('./routes/authRoutes');
-// app.use('/api/auth', authRoutes);
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
 
-// const notesRoutes = require('./routes/notesRoutes');
-// const tasksRoutes = require('./routes/tasksRoutes');
+const notesRoutes = require('./routes/notesRoutes');
+const tasksRoutes = require('./routes/tasksRoutes');
 
-// app.use('/api/notes', notesRoutes);
-// app.use('/api/tasks', tasksRoutes);
+app.use('/api/notes', notesRoutes);
+app.use('/api/tasks', tasksRoutes);
 
 const port= process.env.PORT;
 app.get('/', (req, res) => res.send('Backend is alive!'));
 async function startServer() {
   try {
    
-   //  await sequelize.sync(); 
-   //  console.log("Database synced");
+    await sequelize.sync(); 
+    console.log("Database synced");
     app.listen(process.env.PORT, '0.0.0.0', () => {
       console.log(`Backend running on ${port}`);
     });
