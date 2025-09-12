d7986a8f54392ca581ce09fda2167580365fa0e9
SaiSree <saisreelikhitha9@gmail.com>
2025-09-12 17:25:36 +0530

    Update authService.js

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -5,4 +5,4 @@ const API_URL = `${process.env.REACT_APP_API_URL}/api/auth`;
 export const login = (data) =>  axios.post(`${API_URL}/login`, data);
 export const signup = (data) =>  axios.post(`${API_URL}/signup`, data);
 export const googleLogin = (tokenId) =>
-  axios.post(`${API_URL}google-login`, { tokenId });
+  axios.post(`${API_URL}/google-login`, { tokenId });
