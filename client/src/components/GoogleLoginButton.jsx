99351016c1651e93c7f8dfda3f8a42f9aea823e1
Saico-debs9 <saisreelikhitha9@gmail.com>
2025-09-12 12:57:16 +0530

    Added Google Login

---
+++ client/src/components/GoogleLoginButton.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+import { googleLogin } from "../Services/authService";
+import { useNavigate } from 'react-router-dom';
+
+const GoogleLoginButton = () => {
+  const navigate = useNavigate();
+  const handleCredentialResponse = async (response) => {
+    try {
+      const tokenId = response.credential;
+      
+
+      const res = await googleLogin(tokenId);
+
+      localStorage.setItem("token", res.data.token);
+      navigate('/');
+    } catch (err) {
+      console.error("Google login failed", err);
+      alert("Google login failed. Try again.");
+    }
+  };
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      if (window.google && document.getElementById("googleBtn")) {
+        clearInterval(interval);
+
+        window.google.accounts.id.initialize({
+          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+          callback: handleCredentialResponse,
+        });
+
+        window.google.accounts.id.renderButton(
+          document.getElementById("googleBtn"),
+          { theme: "outline", size: "large" }
+        );
+      }
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div id="googleBtn"></div>;
+};
+
+export default GoogleLoginButton;
